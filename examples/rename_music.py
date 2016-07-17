4adc2eb8fb633649cf1301056b31188a15d71ce5
Josef Friedrich <josef@friedrich.rocks>
2016-07-17 20:00:51 +0200

    Add another example

---
+++ examples/rename_music.py
@@ -0,0 +1,23 @@
+#James Gilles, 2012
+#DWTFYWWI License
+
+from mutagen.easyid3 import EasyID3
+import os
+
+print "input directory for processing: "
+path = raw_input()
+os.chdir(path)
+
+file_list = filter((lambda x: '.mp3' in x), os.listdir(path))
+
+for i in file_list:
+        print "file: "+i
+        try:
+                current = EasyID3(i)
+                newname = current["title"][0] + ".mp3"
+                newname.replace(" ", "_")
+                del current
+                print "renaming "+i+" to "+newname
+                os.rename(i, newname)
+        except:
+print "...that file didn't work for some reason.
