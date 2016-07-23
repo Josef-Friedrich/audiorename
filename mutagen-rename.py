d8b88be30d1d91c792384a044cd68ff683b57252
Josef Friedrich <josef@friedrich.rocks>
2016-07-23 18:35:40 +0200

    Many improvements

--- mutagen-rename.py
+++ mutagen-rename.py
@@ -1,33 +1,56 @@
 #! /usr/bin/env python3
 
-import mutagen, os, sys, argparse
+import mutagen, os, sys, argparse, textwrap
 
-parser = argparse.ArgumentParser(description='Rename music files.')
+old = {}
+new = {}
+
+parser = argparse.ArgumentParser(
+	formatter_class=argparse.RawDescriptionHelpFormatter,
+	description=textwrap.dedent('''\
+		Rename music files from metadata
+
+		Metadata fields:
+			- artist
+			- title
+			- extension
+			- filename
+	'''
+	)
+)
 parser.add_argument('folder', help='A folder containing music')
 
 args = parser.parse_args()
 
-def normalize(audio_file):
-	metadata = {}
-	metadata['extension'] = extension = audio_file.split('.')[-1]
-	metadata['filename'] = os.path.basename(audio_file)
+def map(new_field, old_field=False):
+	if not old_field:
+		old_field = new_field
+	if old_field in old:
+		new[new_field] = str(old[old_field][0])
 
-	m = mutagen.File(audio_file, easy=True)
+def normalize(audio_file):
+	global new
+	new = {}
+	new['extension'] = extension = audio_file.split('.')[-1]
+	new['filename'] = os.path.basename(audio_file)
+
+	global old
+	old = mutagen.File(audio_file, easy=True)
 
 	if extension == 'wma':
-		metadata['artist'] = str(m['WM/ARTISTS'][0])
-	elif extension == 'mp3' or extension == 'm4a':
-		print('lol')
-	if 'title' in m:
-		metadata['title'] = m['title']
-	if 'artist' in m:
-		metadata['artist'] = m['artist']
-	return metadata
+		#print(old)
+		map('artist', 'WM/ARTISTS')
+		map('title', 'Title')
+	elif extension == 'mp3' or extension == 'm4a' or extension == 'flac':
+		map('artist')
+		map('title')
+	return new
 
 for path, subdirs, files in os.walk(args.folder):
 	for audio_file in files:
 		if audio_file.endswith((".mp3", ".m4a", ".flac", ".wma")) == True:
-			metadata = normalize(os.path.join(path, audio_file))
-			print(metadata)
+			new = normalize(os.path.join(path, audio_file))
+			print(new)
+			print(new['artist'] + ' ' + new['title'])
 
 
