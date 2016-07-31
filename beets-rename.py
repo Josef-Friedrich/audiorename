1c233f0668a5cebaa5e5f6db78ad87c2b716ee26
Josef Friedrich <josef@friedrich.rocks>
2016-07-31 22:06:12 +0200

    Test beets standard functions

--- beets-rename.py
+++ beets-rename.py
@@ -2,16 +2,17 @@
 
 from beets import mediafile
 from beets.util.functemplate import Template
+from beets.library import DefaultTemplateFunctions as Functions
 
 lol = mediafile.MediaFile('/home/jf/ownCloud/mutagen/mp3.mp3')
 
 print(lol.title)
 
+t = Template('iii ${lol}l %upper{iii}')
 
-t = Template('iii ${lol}l iii')
-
+f = Functions()
 
 values = {
 	'lol': 'troll'
 }
-print(t.substitute(values))
+print(t.substitute(values, f.functions()))
