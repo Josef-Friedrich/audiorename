b731654b15c7663982d14be9f07affe765a7b27e
Josef Friedrich <josef@friedrich.rocks>
2016-08-02 15:49:10 +0200

    Add description

--- audiorename.py
+++ audiorename.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os, sys, argparse, textwrap
 
@@ -9,16 +10,116 @@ from beets.library import DefaultTemplateFunctions as Functions
 parser = argparse.ArgumentParser(
 	formatter_class=argparse.RawDescriptionHelpFormatter,
 	description=textwrap.dedent('''\
-		Rename audio files from metadata
+Rename audio files from metadata
 
-		Metadata fields:
-			- album
-			- albumartist
-			- artist
-			- extension
-			- filename
-			- title
-			- track
+Metadata fields:
+
+	Ordinary metadata:
+
+		- title
+		- artist
+		- artist_sort:         The “sort name” of the track artist
+		                       (e.g., “Beatles, The” or “White, Jack”).
+		- artist_credit:       The track-specific artist credit name,
+		                       which may be a variation of the artist’s
+		                       “canonical” name.
+		- album
+		- albumartist:         The artist for the entire album, which
+		                       may be different from the artists for the
+		                       individual tracks.
+		- albumartist_sort
+		- albumartist_credit
+		- genre
+		- composer
+		- grouping
+		- year, month, day:    The release date of the specific release.
+		- original_year, original_month, original_day:
+		                       The release date of the original version
+		                       of the album.
+		- track
+		- tracktotal
+		- disc
+		- disctotal
+		- lyrics
+		- comments
+		- bpm
+		- comp:                 Compilation flag.
+		- albumtype:            The MusicBrainz album type; the
+		                        MusicBrainz wiki has a list of type
+		                        names.
+		- label
+		- asin
+		- catalognum
+		- script
+		- language
+		- country
+		- albumstatus
+		- media
+		- albumdisambig
+		- disctitle
+		- encoder
+
+	Audio information:
+
+		- length                (in seconds)
+		- bitrate               (in kilobits per second, with units:
+		                        e.g., “192kbps”)
+		- format                (e.g., “MP3” or “FLAC”)
+		- channels
+		- bitdepth              (only available for some formats)
+		- samplerate            (in kilohertz, with units: e.g.,
+		                        “48kHz”)
+
+	MusicBrainz and fingerprint information:
+
+		- mb_trackid
+		- mb_albumid
+		- mb_artistid
+		- mb_albumartistid
+		- mb_releasegroupid
+		- acoustid_fingerprint
+		- acoustid_id
+
+
+	Template Functions
+
+		- %lower{text}:         Convert text to lowercase.
+		- %upper{text}:         Convert text to UPPERCASE.
+		- %title{text}:         Convert text to Title Case.
+		- %left{text,n}:        Return the first n characters of text.
+		- %right{text,n}:       Return the last n characters of text.
+		- %if{condition,text} or %if{condition,truetext,falsetext}:
+		                        If condition is nonempty (or nonzero,
+		                        if it’s a number), then returns the
+		                        second argument. Otherwise, returns the
+		                        third argument if specified (or nothing
+		                        if falsetext is left off).
+		- %asciify{text}:       Convert non-ASCII characters to their
+		                        ASCII equivalents. For example, “café”
+		                        becomes “cafe”. Uses the mapping
+		                        provided by the unidecode module.
+		- %aunique{identifiers,disambiguators}:
+		                        Provides a unique string to disambiguate.
+		- %time{date_time,format}:
+		                        Return the date and time in any format
+		                        accepted by strftime. For example, to
+		                        get the year some music was added to
+		                        your library, use %time{$added,%Y}.
+		- %first{text}:         Returns the first item, separated by ; .
+		                        You can use %first{text,count,skip},
+		                        where count is the number of items
+		                        (default 1) and skip is number to skip
+		                        (default 0). You can also use
+		                        %first{text,count,skip,sep,join} where
+		                        sep is the separator, like ; or / and
+		                        join is the text to concatenate the
+		                        items.
+		- %ifdef{field}, %ifdef{field,truetext} or %ifdef{field,truetext,falsetext}:
+		                        If field exists, then return truetext or
+		                        field (default). Otherwise, returns
+		                        falsetext. The field should be entered
+		                        without $.
+
 	'''
 	)
 )
