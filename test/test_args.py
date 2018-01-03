1a773bb373128960ee87706101d32fd0ed4c399d
Josef Friedrich <josef@friedrich.rocks>
2018-01-03 10:05:03 +0100

    Better test for default arguments

--- test/test_args.py
+++ test/test_args.py
@@ -122,30 +122,101 @@ class TestHelp(unittest.TestCase):
 
 class TestArgsDefault(unittest.TestCase):
 
-    def test_defaults(self):
+    def setUp(self):
         from audiorename.args import parse_args
-        args = parse_args(['lol'])
-        self.assertEqual(args.source, 'lol')
+        from audiorename.args import ArgsDefault
+        self.default = ArgsDefault()
+        self.default.source = 'lol'
+        self.args = parse_args(['lol'])
 
-        self.assertEqual(args.classical, False)
-        self.assertEqual(args.compilation, False)
-        self.assertEqual(args.copy, False)
-        self.assertEqual(args.delete_existing, False)
-        self.assertEqual(args.dry_run, False)
-        self.assertEqual(args.extension, 'mp3,m4a,flac,wma')
-        self.assertEqual(args.filter_album_complete, False)
-        self.assertEqual(args.filter_album_min, False)
-        self.assertEqual(args.format, False)
-        self.assertEqual(args.mb_track_listing, False)
-        self.assertEqual(args.move, False)
-        self.assertEqual(args.shell_friendly, False)
-        self.assertEqual(args.skip_if_empty, False)
-        self.assertEqual(args.soundtrack, False)
-        self.assertEqual(args.source_as_target_dir, False)
-        self.assertEqual(args.target_dir, '')
-        self.assertEqual(args.unittest, False)
-        self.assertEqual(args.verbose, False)
-        self.assertEqual(args.work, False)
+    def test_source(self):
+        self.assertEqual(self.args.source, 'lol')
+        self.assertEqual(self.args.source, self.default.source)
+
+    def test_classical(self):
+        self.assertEqual(self.args.classical, False)
+        self.assertEqual(self.args.classical, self.default.classical)
+
+    def test_compilation(self):
+        self.assertEqual(self.args.compilation, False)
+        self.assertEqual(self.args.compilation, self.default.compilation)
+
+    def test_copy(self):
+        self.assertEqual(self.args.copy, False)
+        self.assertEqual(self.args.copy, self.default.copy)
+
+    def test_delete_existing(self):
+        self.assertEqual(self.args.delete_existing, False)
+        self.assertEqual(self.args.delete_existing,
+                         self.default.delete_existing)
+
+    def test_dry_run(self):
+        self.assertEqual(self.args.dry_run, False)
+        self.assertEqual(self.args.dry_run, self.default.dry_run)
+
+    def test_extension(self):
+        self.assertEqual(self.args.extension, 'mp3,m4a,flac,wma')
+        self.assertEqual(self.args.extension, self.default.extension)
+
+    def test_filter_album_complete(self):
+        self.assertEqual(self.args.filter_album_complete, False)
+        self.assertEqual(self.args.filter_album_complete,
+                         self.default.filter_album_complete)
+
+    def test_filter_album_min(self):
+        self.assertEqual(self.args.filter_album_min, False)
+        self.assertEqual(self.args.filter_album_min,
+                         self.default.filter_album_min)
+
+    def test_format(self):
+        self.assertEqual(self.args.format, False)
+        self.assertEqual(self.args.format, self.default.format)
+
+    def test_job_info(self):
+        self.assertEqual(self.args.job_info, False)
+        self.assertEqual(self.args.job_info, self.default.job_info)
+
+    def test_mb_track_listing(self):
+        self.assertEqual(self.args.mb_track_listing, False)
+        self.assertEqual(self.args.mb_track_listing,
+                         self.default.mb_track_listing)
+
+    def test_move(self):
+        self.assertEqual(self.args.move, False)
+        self.assertEqual(self.args.move, self.default.move)
+
+    def test_shell_friendly(self):
+        self.assertEqual(self.args.shell_friendly, False)
+        self.assertEqual(self.args.shell_friendly, self.default.shell_friendly)
+
+    def test_skip_if_empty(self):
+        self.assertEqual(self.args.skip_if_empty, False)
+        self.assertEqual(self.args.skip_if_empty, self.default.skip_if_empty)
+
+    def test_soundtrack(self):
+        self.assertEqual(self.args.soundtrack, False)
+        self.assertEqual(self.args.soundtrack, self.default.soundtrack)
+
+    def test_source_as_target_dir(self):
+        self.assertEqual(self.args.source_as_target_dir, False)
+        self.assertEqual(self.args.source_as_target_dir,
+                         self.default.source_as_target_dir)
+
+    def test_target_dir(self):
+        self.assertEqual(self.args.target_dir, '')
+        self.assertEqual(self.args.target_dir, self.default.target_dir)
+
+    def test_unittest(self):
+        self.assertEqual(self.args.unittest, False)
+        self.assertEqual(self.args.unittest, self.default.unittest)
+
+    def test_verbose(self):
+        self.assertEqual(self.args.verbose, False)
+        self.assertEqual(self.args.verbose, self.default.verbose)
+
+    def test_work(self):
+        self.assertEqual(self.args.work, False)
+        self.assertEqual(self.args.work, self.default.work)
 
 
 if __name__ == '__main__':
