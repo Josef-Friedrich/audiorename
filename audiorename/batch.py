6ddfca5f77fbe88118ea66946b34889c26f05f3f
Josef Friedrich <josef@friedrich.rocks>
2018-01-04 21:47:53 +0100

    Add documentation to batch.py

--- audiorename/batch.py
+++ audiorename/batch.py
@@ -9,12 +9,21 @@ import os
 
 
 class Batch(object):
+    """This class first sorts all files and then walks through all files. At
+    this process it tries to make bundles of files belonging to a album. This
+    bundle of files is temporary stores in the attribute `virtual_album`. This
+    complicated mechanism is needed for the both filters `album_complete` and
+    `album_min`."""
+
+    virtual_album = []
+    """Storage of a list of files belonging to an album."""
+
+    current_album_title = ''
+    """Storage for the album title of the current audio file."""
 
     def __init__(self, job):
         self.job = job
         self.bundle_filter = job.filter.album_complete or job.filter.album_min
-        self.album = []
-        self.album_title = ''
 
     def check_extension(self, path):
         """Check the extension of the track.
@@ -32,7 +41,7 @@ class Batch(object):
         """Compare the number of tracks in an album with the minimal track
         threshold.
         """
-        if len(self.album) > int(self.job.filter.album_min):
+        if len(self.virtual_album) > int(self.job.filter.album_min):
             return True
         else:
             return False
@@ -40,16 +49,16 @@ class Batch(object):
     def check_completeness(self):
         """Check if the album is complete"""
         max_track = 0
-        for record in self.album:
+        for record in self.virtual_album:
             if record['track'] > max_track:
                 max_track = record['track']
 
-        if len(self.album) == max_track:
+        if len(self.virtual_album) == max_track:
             return True
         else:
             return False
 
-    def check_album(self):
+    def process_album(self):
         """Check an album for quantity and completeness."""
         quantity = True
         completeness = True
@@ -59,17 +68,17 @@ class Batch(object):
             completeness = False
 
         if quantity and completeness:
-            for p in self.album:
+            for p in self.virtual_album:
                 do_rename(p['path'], job=self.job)
 
-        self.album = []
+        self.virtual_album = []
 
     def make_bundles(self, path=''):
         """
         :params str path: The path of the tracks.
         """
         if not path:
-            self.check_album()
+            self.process_album()
             return
 
         try:
@@ -78,10 +87,11 @@ class Batch(object):
             record['title'] = media.album
             record['track'] = media.track
             record['path'] = path
-            if not self.album_title or self.album_title != media.album:
-                self.album_title = media.album
-                self.check_album()
-            self.album.append(record)
+            if not self.current_album_title or \
+                    self.current_album_title != media.album:
+                self.current_album_title = media.album
+                self.process_album()
+            self.virtual_album.append(record)
         except phrydy.mediafile.UnreadableFileError:
             pass
 
