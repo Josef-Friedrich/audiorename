4f5c145773af69088f4e51be89fc7c656e828231
rokdd <640866+rokdd@users.noreply.github.com>
2022-02-08 15:14:39 +0100

    Update audiofile.py

--- audiorename/audiofile.py
+++ audiorename/audiofile.py
@@ -347,7 +347,7 @@ def do_job_on_audiofile(source, job=None):
             job.metadata_actions.enrich_metadata,
             job.metadata_actions.remap_classical
         )
-    elif source.meta.genre.lower() in job.filter.genre_classical:
+    elif getattr(source.meta,"genre","").lower() in job.filter.genre_classical:
         action.metadata(
             source,
             job.metadata_actions.enrich_metadata,
