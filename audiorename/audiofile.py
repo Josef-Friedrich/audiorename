7615d5101c3a8c2a984789eb58e8830bc69d9646
rokdd <640866+rokdd@users.noreply.github.com>
2022-02-08 11:37:35 +0100

    Update audiofile.py

--- audiorename/audiofile.py
+++ audiorename/audiofile.py
@@ -339,6 +339,9 @@ def do_job_on_audiofile(source, job=None):
     ##
     # Metadata actions
     ##
+    
+    if source.meta.genre in ["Classical","Classical Instrumental","Classical Ballet","Classical Piano","Klassische Musik","Piano","Klavier"]:
+        job.metadata_actions.remap_classical=True
 
     if job.metadata_actions.remap_classical or \
             job.metadata_actions.enrich_metadata:
