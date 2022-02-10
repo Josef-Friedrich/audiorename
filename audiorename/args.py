bda40065973faac46905a8708b4beecb29e5bdba
rokdd <640866+rokdd@users.noreply.github.com>
2022-02-10 18:01:37 +0100

    Update args.py

--- audiorename/args.py
+++ audiorename/args.py
@@ -102,7 +102,7 @@ class ArgsDefault():
     dry_run = False
     enrich_metadata = False
     extension = 'mp3,m4a,flac,wma'
-    genre_classical = 'classical,'
+    genre_classical = ','
     field_skip = False
     format = False
     job_info = False
