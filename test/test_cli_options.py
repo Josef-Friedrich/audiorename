d0e7cab8ac39979a8b2b2d238f4f7e92eb8e379f
rokdd <640866+rokdd@users.noreply.github.com>
2022-02-20 20:47:44 +0100

    Add a test for the option --format-classical (#7)

--- test/test_cli_options.py
+++ test/test_cli_options.py
@@ -453,6 +453,24 @@ class TestSkipIfEmpty(unittest.TestCase):
         self.assertTrue(helper.has(self.compilation, 'Dry run'))
 
 
+# --classical_format string
+class TestClassicalFormat(unittest.TestCase):
+
+    def assertDryRun(self, folder, track, test):
+        self.assertEqual(helper.dry_run([
+            '--classical', '--format-classical', '$ar_combined_composer/'
+            '${ar_combined_disctrack}_%shorten{$ar_classical_title,64}_'
+            '%shorten{$acoustid_id,8}',
+            helper.get_testfile('classical', folder, track)
+        ]), test)
+
+    def test_debussy_01(self):
+        self.assertDryRun(
+            'Debussy_Estampes-etc', '01.mp3',
+            '/Debussy_Claude/01_Pagodes_.mp3'
+        )
+
+
 # --format
 class TestCustomFormats(unittest.TestCase):
 
