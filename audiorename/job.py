85b65d66f7669a4bacfd960f116dfbdd8978a039
rokdd <rokdd@web.de>
2022-02-22 10:46:01 +0100

    Fix

--- audiorename/job.py
+++ audiorename/job.py
@@ -121,7 +121,7 @@ class Formats(object):
             defaults.soundtrack = args.soundtrack
 
         if args.format_classical:
-        	print(args.format_classical)
+            print(args.format_classical)
             defaults.classical = args.format_classical
 
         self.classical=defaults.classical
